5aed92d4f1462c29248712a25908007eb6fabece
Alejandro Colodrero Garcia <garciacolodreroalex@outlook.com>
2024-10-30 10:16:04 +0100

    Ejercicios 1,2
    Probando

---
+++ DWES/Bloque A/A_1.1.php
@@ -0,0 +1,23 @@
+<?php
+
+// Your PHP code starts here
+$name = "Alex"; 
+$price = "2"; 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <h1>The candy Store</h1>
+    <h2>Welcome <?php echo $name;?> </h2>
+    <p>The cost of the candy is 
+    <?php echo $price;?> per pack.
+    </p>
+</body>
+</html>
